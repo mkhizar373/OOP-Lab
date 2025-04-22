999220aa7dc90c40e13a9fcb53ba3dae06d764c1
mkhizar373 <khizarrashid856@gmail.com>
2025-04-22 23:04:00 +0500

    Create

---
+++ Lab9/Task3/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include "LectureCourse.h"
+#include "LabCourse.h"
+using namespace std;
+
+int main() {
+    Course* course1 = new LectureCourse("CS1001", 3, 70, 75);
+    Course* course2 = new LabCourse("CL1001", 1, 85, 80);
+
+    course1->calculateGrade();
+    course1->displayInfo();
+
+    cout<<endl;
+
+    course2->calculateGrade();
+    course2->displayInfo();
+
+    delete course1;
+    delete course2;
+
+    return 0;
+}
